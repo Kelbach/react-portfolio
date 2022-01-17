33d430397a88e34dc304b3982c97fb7b161a7461
kelbach <87092340+Kelbach@users.noreply.github.com>
2022-01-16 21:39:44 -0500

    resume section functionality complete

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 // import { Document, Page } from 'react-pdf';
 
 function Resume() {
-    return(
-        <div>Resume Coming Soon!</div>
-    )
+    return (
+        <section id="about-me">
+          <div className="title">
+            <h2>Resume</h2>
+          </div>
+          <article>
+            <p>Proficiencies: </p>
+            <ol>
+                <li>Javascript:
+                    <ul>
+                        <li>MERN stack</li>
+                        <li>Progressive Web Apps</li>
+                        <li>SQL, MVC, ORM</li>
+                        <li>Express, Node, API's</li>
+                    </ul>
+                </li>
+                <li>HTML</li>
+                <li>CSS</li>
+            </ol>
+            <p>Below is my old Resume. The new is in the works!</p>
+            <a href={require("./20190826_kelbach_resume_science.pdf")} download="Resume2019">Download my Old Resume here</a>
+          </article>
+        </section>
+    );
 //   const [numPages, setNumPages] = useState(null);
 //   const [pageNumber, setPageNumber] = useState(1);
 
